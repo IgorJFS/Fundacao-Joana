8e29551e6b653fdf0acb19710f1e1d7f7c27dad6
IgorJFS <igormpaz@gmail.com>
2025-12-08 04:25:13 -0300

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -81,6 +82,7 @@ export default function RootLayout({
       >
         <Toaster position="top-right" richColors />
         {children}
+        <Analytics />
       </body>
     </html>
   );
